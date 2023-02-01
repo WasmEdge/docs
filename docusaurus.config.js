123b7ad3549f40f8bc063ea1b0bb0511d5a3303d
JYC0413 <juyichen0413@foxmail.com>
2023-02-01 16:32:49 +0800

    remove docs & book in url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,144 +6,138 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'WasmEdge Book',
-  tagline: 'Bring the cloud-native and serverless application paradigms to Edge Computing.',
-  url: 'https://alabulei1.github.io/',
-  baseUrl: '/book/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    title: 'WasmEdge Book',
+    tagline: 'Bring the cloud-native and serverless application paradigms to Edge Computing.',
+    url: 'https://alabulei1.github.io/',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  favicon: 'img/favicon.ico',
+    favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'Second State', // Usually your GitHub org/user name.
+    projectName: 'WasmEdge', // Usually your repo name.
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en','cn'],
-    localeConfigs: {
-      en: {label: 'English'},
-      cn: {label: '简体中文'}
-    }
-  },
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'zh-Hans'],
+        localeConfigs: {
+            en: {label: 'English'},
+            "zh-Hans": {label: '简体中文'}
+        }
+    },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/alabulei1/book',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/alabulei1/book',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    routeBasePath: '/',
+                    editUrl:
+                        'https://github.com/alabulei1/book',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'WasmEdge',
-        logo: {
-          alt: 'WasmEdge Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'develop-guide/overview',
-            position: 'left',
-            label: 'Develop',
-          },{
-            type: 'doc',
-            docId: 'embed-guide/overview',
-            position: 'left',
-            label: 'Embed',
-          },{
-            type: 'doc',
-            docId: 'contribute-guide/overview',
-            position: 'left',
-            label: 'Contribute',
-          },
-          {
-            type: 'localeDropdown',
-            position: 'right',
-            dropdownItemsBefore: [],
-            dropdownItemsAfter: [
-              {
-                href: 'https://translate.google.com/',
-                label: 'Translate',
-                target: '_blank',
-                rel: null,
-              },
-            ],
-            className: 'icon-link language navbar__item',
-          },
-          {
-            href: 'https://github.com/WasmEdge/WasmEdge',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Develop',
-                to: '/docs/develop-guide/overview',
-              },{
-                label: 'Embed',
-                to: '/docs/embed-guide/overview',
-              },{
-                label: 'Contribute',
-                to: '/docs/contribute-guide/overview',
-              }
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Discord',
-                href: 'https://discord.gg/U4B5sFTkFc',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/realwasmedge',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} WasmEdge. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        ({
+            navbar: {
+                title: 'WasmEdge',
+                logo: {
+                    alt: 'WasmEdge Logo',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {
+                        type: 'doc',
+                        docId: 'develop-guide/overview',
+                        position: 'left',
+                        label: 'Develop',
+                    }, {
+                        type: 'doc',
+                        docId: 'embed-guide/overview',
+                        position: 'left',
+                        label: 'Embed',
+                    }, {
+                        type: 'doc',
+                        docId: 'contribute-guide/overview',
+                        position: 'left',
+                        label: 'Contribute',
+                    },
+                    {
+                        type: 'localeDropdown',
+                        position: 'right',
+                        dropdownItemsBefore: [],
+                        dropdownItemsAfter: [
+                            {
+                                href: 'https://translate.google.com/',
+                                label: 'Translate',
+                                target: '_blank',
+                                rel: null,
+                            },
+                        ],
+                        className: 'icon-link language navbar__item',
+                    },
+                    {
+                        href: 'https://github.com/WasmEdge/WasmEdge',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Docs',
+                        items: [
+                            {
+                                label: 'Develop',
+                                to: '/docs/develop-guide/overview',
+                            }, {
+                                label: 'Embed',
+                                to: '/docs/embed-guide/overview',
+                            }, {
+                                label: 'Contribute',
+                                to: '/docs/contribute-guide/overview',
+                            }
+                        ],
+                    },
+                    {
+                        title: 'Community',
+                        items: [
+                            {
+                                label: 'Discord',
+                                href: 'https://discord.gg/U4B5sFTkFc',
+                            },
+                            {
+                                label: 'Twitter',
+                                href: 'https://twitter.com/realwasmedge',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} WasmEdge. Built with Docusaurus.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
 };
 
 module.exports = config;
