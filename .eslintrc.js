273c9ea331efefc6376f868fabf0d70f7045b47b
Adithya Krishna <adikrish@redhat.com>
2023-05-13 15:21:43 +0530

    Added Eslint
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  plugins: ['react', 'jsx-a11y', 'import', 'prettier', 'react-hooks', 'regexp'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': ['error', {}, {usePrettierrc: true}],
+    'react/prop-types': 'off',
+    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}],
+    'react/jsx-props-no-spreading': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'regexp/no-unused-capturing-group': 'error',
+    'regexp/no-useless-backreference': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.json'],
+      },
+    },
+  },
+};
