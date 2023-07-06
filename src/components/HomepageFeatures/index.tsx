fb7cc09af07e1ecc282df854ceb503bbc8bdbc25
JYC0413 <juyichen0413@foxmail.com>
2023-07-06 19:33:26 +0800

    Update index.tsx
    fix
    
    Signed-off-by: JYC0413 <juyichen0413@foxmail.com>

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -6,42 +6,42 @@ import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
 
 const FeatureList = [
-    {
-        id: 0,
-        title: 'Develop Wasm applications',
-        LinkUrl: '/develop/overview',
-        Svg: require('@site/static/img/programmable.svg').default,
-        description: (
-            <>
-                Develop Wasm apps in Rust, JavaScript, Go, and Python. Those apps are
-                compiled into Wasm and run inside the WasmEdge sandbox.
-            </>
-        ),
-    },
-    {
-        id: 1,
-        title: 'Embed Wasm functions into a host',
-        LinkUrl: '/embed/overview',
-        Svg: require('@site/static/img/WebAssembly.svg').default,
-        description: (
-            <>
-                The host app could be written in Rust, C/C++, Go, and Java. Wasm
-                functions are plugged in without re-compiling the host.
-            </>
-        ),
-    },
-    {
-        id: 2,
-        title: 'Extend WasmEdge',
-        LinkUrl: '/contribute/overview',
-        Svg: require('@site/static/img/lowcode.svg').default,
-        description: (
-            <>
-                Create WasmEdge extensions and plugins in Rust and C/C++. Contribute to
-                the WasmEdge project.
-            </>
-        ),
-    },
+  {
+    id: 0,
+    title: 'Develop Wasm applications',
+    LinkUrl: '/develop/overview',
+    Svg: require('@site/static/img/programmable.svg').default,
+    description: (
+      <>
+        Develop Wasm apps in Rust, JavaScript, Go, and Python. Those apps are
+        compiled into Wasm and run inside the WasmEdge sandbox.
+      </>
+    ),
+  },
+  {
+    id: 1,
+    title: 'Embed Wasm functions into a host',
+    LinkUrl: '/embed/overview',
+    Svg: require('@site/static/img/WebAssembly.svg').default,
+    description: (
+      <>
+        The host app could be written in Rust, C/C++, Go, and Java. Wasm
+        functions are plugged in without re-compiling the host.
+      </>
+    ),
+  },
+  {
+    id: 2,
+    title: 'Extend WasmEdge',
+    LinkUrl: '/contribute/overview',
+    Svg: require('@site/static/img/lowcode.svg').default,
+    description: (
+      <>
+        Create WasmEdge extensions and plugins in Rust and C/C++. Contribute to
+        the WasmEdge project.
+      </>
+    ),
+  },
 ];
 
 function Feature({ Svg, LinkUrl, title, description }) {
@@ -51,7 +51,7 @@ function Feature({ Svg, LinkUrl, title, description }) {
         <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className={`${styles.hoverStyle} text--center padding-horiz--md`}>
-        <div style={{display: 'flex' }}>
+        <div style={{ display: 'flex' }}>
           <Link to={LinkUrl}>
             <h3>{title}</h3>
           </Link>
